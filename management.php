aacfc138ec04f2857a2adce0784473ce7bf0af46
zorbey-qcow2 <zorbey@tuta.io>
2021-08-07 20:08:38 +0300

    usercp

--- management.php
+++ management.php
@@ -1,3 +1,23 @@
 <?php
-echo "Test"
+
+$ManageHeadBody = file_get_contents("gorunum/management_head_body.php");
+$navbarCik = file_get_contents("gorunum/navbar_unlogin.php");
+$navbarLogged = file_get_contents("gorunum/navbar_loggedin.php");
+$manageView = file_get_contents("gorunum/management_view.php");
+
+echo $ManageHeadBody;
+echo $navbarLogged;
+echo $manageView;
+
 ?>
+
+<script async type="text/javascript" src="gorunum/js/bulma.js"></script>
+</body>
+
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+            <strong>Coded</strong> by <a href="https://">z</a>.
+        </p>
+    </div>
+</footer>
